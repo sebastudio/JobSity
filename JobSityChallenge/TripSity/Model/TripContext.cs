0d4345278e1524dbc59b09acf467183cb450d669
sebastudio <81712214+sebastudio@users.noreply.github.com>
2021-08-31 15:00:14 -0300

    Add files via upload

---
+++ JobSityChallenge/TripSity/Model/TripContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System;
+
+namespace TripSity
+{
+    public class TripContext : DbContext
+    {
+        public TripContext(DbContextOptions<TripContext> options)
+    : base(options)
+        {
+        }
+
+        public DbSet<TripItem> Items { get; set; }
+
+        static public List<TripItem> Trips = Seed();
+
+        static List<TripItem> Seed()
+        {
+            List<TripItem> trips = new();
+            foreach(string s in TripSity.Files.TripSeed.Trips)
+            {
+                TripItem item = new()
+                {
+                    Region = s.Substring(0,7),
+                    DateTimeTrip = Convert.ToDateTime(s.Substring(8,16).Replace('_',' ').Trim()),
+                    DataSource = s[24..].Trim()
+                };
+                trips.Add(item);
+            }
+            return trips;
+        }
+
+    }
+}
